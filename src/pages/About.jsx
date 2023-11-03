780d9c9154b17eb2e138fed73058b58db219e122
Arda Irmak <ardairmak2003@gmail.com>
2023-11-03 11:26:04 +0300

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Wrapper from "../assets/wrappers/AboutPage";
+const About = () => {
+  return (
+    <Wrapper>
+      <h3>About Us</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente ullam,
+        unde, nemo quia, dignissimos maiores excepturi molestias velit in
+        laudantium quos placeat autem cumque possimus provident repellendus
+        accusantium quam mollitia?
+      </p>
+    </Wrapper>
+  );
+};
+
+export default About;
